065409ec6abc2a802d5ec5a8c017a1625d1231d5
Viet An <vietanisme@gmail.com>
2018-02-02 07:38:52 +0700

    add gitignore

--- sample/core.py
+++ sample/core.py
@@ -1,45 +1,35 @@
 import sys
 import pygame
 import time
-from packages.ball.ball import Ball
+from packages.ball.ball import Ball, vector, point
 from packages.env_var.env_var import *
 
-def calc_frame_delay(time):
-    h = pygame.display.Info().current_h
-    x_in_a_sec = h / time
-    return 1 / x_in_a_sec
+# pygame.init()
+# font = pygame.font.SysFont('Consolas', 50)
+# screen = pygame.display.set_mode(screen_size, 0)
+# pygame.display.set_caption('Game')
+# screen.fill(background)
+# pygame.display.update()
+# still_playing = True
 
-pygame.init()
-font = pygame.font.SysFont('Consolas', 50)
-screen = pygame.display.set_mode(screen_size, 0)
-pygame.display.set_caption('Game')
-screen.fill(background)
-pygame.display.update()
-
-while still_playing:
-    fall_time = 10
-    frame_delay = calc_frame_delay(fall_time)
-    spawn_delay = 5
-    accelerate_delay = 10
-    last_spawn_time = last_accelerate_time = last_frame_time = time.time()
-    last_spawn_time -= 5
-    ball_list = []
-    still_playing = True
-    session = True
-    while session:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-        else:
-            this_time = time.time()
-            if this_time - last_frame_time >= frame_delay:
-                screen.fill(background)
-                for id in range (len (ball_list)):
-                    res = ball_list[id].fall (screen)
-                    if not res:
-                        del_id
-            if this_time - last_spawn_time >= spawn_delay:
-
-
-                
+# while still_playing:
+#     last_frame_time = time.time()
+#     ball = Ball ()
+#     session = True
+#     while session:
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 sys.exit()
+#         else:
+#             this_time = time.time()
+#             if this_time - last_frame_time >= frame_delay:
+#                 screen.fill(background)
+#                 for id in range (len(balls) - 1, -1, -1):
+#                     a = balls[id].fall ()
+#                     a
+#             if this_time - last_spawn_time >= spawn_delay:
+a = vector (randspeed = 20)
+print (a)
+pygame.quit()
+sys.exit()
--- sample/packages/ball/ball.py
+++ sample/packages/ball/ball.py
@@ -1,10 +1,45 @@
 import pygame
 import sys
 import random
+import math
 from ..env_var.env_var import *
 from pygame.locals import *
-def text_pos (pos):
+
+
+def text_pos(pos):
     return [pos[0] - text_recal[0], pos[1] - text_recal[1]]
+
+
+class vector (object):
+    def __init__(self, x = 0, y = 0, randspeed = 0):
+        if randspeed:
+            self.randomize (randspeed)
+        else:
+            self.x, self.y = x, y
+
+    def __add__(self, a):
+        rx, ry = self.x + a.x, self.y + a.y
+        return vector(rx, ry)
+
+    def __radd__(self, a):
+        rx, ry = self.x + a.x, self.y + a.y
+        return vector(rx, ry)
+
+    def __str__(self):
+        return ('({} ; {})'.format(self.x, self.y))
+
+    def getpos(self):
+        return (self.x, self.y)
+
+    def randomize(self, speed):
+        self.x = random.randrange (0, speed + 1)
+        self.y = round(math.sqrt (speed * speed - self.x * self.x))
+
+
+class point (vector):
+    pass
+
+
 class Ball (object):
     def __init__(self):
         random.seed()
@@ -13,19 +48,22 @@ class Ball (object):
         self.color = colors['blue']
         self.font = pygame.font.SysFont('Consolas', 20)
 
-    def fall(self, screen):
-        pre_pos = self.pos
-        self.pos[1] += self.step
+    def move(self, screen):
+        self.pos += self.step
         if self.pos[1] + radius >= screen.get_height():
-        	return False
+            return False
         else:
             pygame.draw.circle(screen, self.color, self.pos, radius)
-            text_box = self.font.render (self.text, True, colors['red'])
-            screen.blit (text_box, text_pos (self.pos))
+            text_box = self.font.render(self.text, True, colors['red'])
+            screen.blit(text_box, text_pos(self.pos))
             pygame.display.update()
             return True
 
-    def create(self, screen, x, step):
-        self.pos = [x, radius - step]
+    def create(self, screen, step):
+        random.seed()
+        h, w = screen.get_height(), screen.get_width()
+        x = random.randrange (radius + 1, w - radius)
+        y = random.randrange (radius + 1, h - radius)
+        self.pos = point (x, y)
         self.step = step
         self.fall(screen)
